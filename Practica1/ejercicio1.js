47c24b87b851dafe65b8f6a38c3de4ac5fa5867d
Danel Rolon <danelrolon10@gmail.com>
2025-05-16 23:29:40 -0600

    ejercicio 1 - practica 1

---
+++ Practica1/ejercicio1.js
@@ -0,0 +1,14 @@
+// Declaramos una variable llamada 'nombre' y le asignamos el valor "Armando"
+let nombre = "Armando";
+
+// Declaramos una constante llamada 'edad' y le asignamos el valor 25
+const edad = 25;
+
+// Cambiamos el valor de la variable 'nombre' a "Ana Maria"
+nombre = "Ana Maria";
+
+// Creamos una constante 'saludo' que concatena un mensaje usando 'nombre' y 'edad'
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+// Mostramos el saludo en la consola
+console.log(saludo);
