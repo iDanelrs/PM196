b509b3c9db15167e1a3b5f2cd996b59a1052a69d
Danel Rolon <danelrolon10@gmail.com>
2025-05-16 23:30:10 -0600

    ejercicio 2 - practica 1

---
+++ Practica1/ejercicio2.js
@@ -0,0 +1,11 @@
+// Definimos una función arrow que calcula el cuadrado de un número
+const cuadrado = (numero) => numero * numero;
+
+// Mostramos en consola el resultado de elevar 3 al cuadrado (9)
+console.log(cuadrado(3)); // 9
+
+// Mostramos en consola el resultado de elevar 5 al cuadrado (25)
+console.log(cuadrado(5)); // 25
+
+// Mostramos en consola el resultado de elevar 10 al cuadrado (100)
+console.log(cuadrado(10)); // 100
