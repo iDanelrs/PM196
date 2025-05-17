475706b8623231d21759b1c9e6ad596fa002c106
Danel Rolon <danelrolon10@gmail.com>
2025-05-16 23:32:54 -0600

    ejercicio 3 - practica 1

---
+++ Practica1/ejercicio3.js
@@ -0,0 +1,5 @@
+// Definimos una función flecha que recibe un nombre y una edad, y devuelve un saludo personalizado
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+// Mostramos en consola el saludo personalizado usando los valores "Danel" y 21
+console.log(saludoPersonalizado("Danel", 21));
